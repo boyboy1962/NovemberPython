5e5608beab8e7664c4cb915e487b7001bdafe3b5
Master <boyboy1926@gmail.com>
2021-12-03 19:00:56 +0900

    수업시작전 수정
    Test09
    문제에 따라 모두 잘 작동한다.
    여기에 유저에게 어느정도 크기를 원하는지 물어보아서 크기를 자유자제로 바꿀 수 있게 준비하였다.

--- Day06/Test09.py
+++ Day06/Test09.py
@@ -1,15 +1,17 @@
+numberOfLines = int(input("라인의 수를 정해주세요: "))
+
 # 문제 1)
 """
 #
 ##
 ###
 """
-
-for i in range(0,3):
+print("=" * numberOfLines)
+for i in range(0,numberOfLines):
     for j in range(i + 1): # j의 끝값: 1(미만) #ㅓ의 끝값 : 2(미만)
         print("#",end="")
     print()
-
+print("=" * numberOfLines)
 
 #문제 2)
 """
@@ -17,11 +19,12 @@ for i in range(0,3):
 ##
 #
 """
-
-for i in range(0,3):
-    for j in range(3 - i):
+print("=" * numberOfLines)
+for i in range(0,numberOfLines):
+    for j in range(numberOfLines - i):
         print("#",end="")
     print()
+print("=" * numberOfLines)
 
 #문제 3)
 """
@@ -29,13 +32,14 @@ for i in range(0,3):
 @@#
 @@@
 """
-
-for i in range(0,3):
+print("=" * numberOfLines)
+for i in range(0,numberOfLines):
     for j in range(i + 1):
         print("@",end="")
-    for j in range(2 - i):
+    for j in range((numberOfLines - 1) - i):
         print("#",end="") 
     print()
+print("=" * numberOfLines)
 
 
 #문제 4)
@@ -44,9 +48,9 @@ for i in range(0,3):
  ###
 #####
 """
-
-for i in range(0,3):
-    for j in range(2 - i):
+print("=" * (numberOfLines * 2 - 1 ))
+for i in range(0,numberOfLines):
+    for j in range((numberOfLines - 1) - i):
         print(" ",end="")
     for j in range(i + 1):
         print("#",end="")
